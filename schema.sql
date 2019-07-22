19ab970edcfbb1c5cf2c245cda58ed60e69f4cb7
FabianB14 <32110808+FabianB14@users.noreply.github.com>
2019-07-22 16:35:54 -0700

    database is up and running

--- schema.sql
+++ schema.sql
@@ -1,15 +1,21 @@
-DROP TABLE IF EXISTS citys;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS cities;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   user_email VARCHAR(20)
 );
 
-CREATE TABLE citys(
+CREATE TABLE cities(
   id SERIAL PRIMARY KEY,
   city_name VARCHAR(255),
-  city_geocode_id VARCHAR(255),
-  user_id INTEGER NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users (id)
+  city_geocode_id VARCHAR(255)
 );
+
+CREATE TABLE favorites (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  city_id INTEGER NOT NULL,
+  FOREIGN KEY (city_id) REFERENCES cities (id),
+  FOREIGN KEY (user_id) REFERENCES users (id)
+)
