388393e61c22acd4e371461202c2d79ed6361288
Peter Lee <leepj85@gmail.com>
2019-07-22 13:30:11 -0700

    created files and npm install

---
+++ server.js
@@ -0,0 +1,27 @@
+'use strict'
+
+//Application Dependencies
+const express = require('express');
+const superagent = require('superagent');
+const pg = require('pg');
+const cors = require('cors');
+
+// Load environment variables from .env file
+require('dotenv').config();
+
+// Application Setup
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+// Database Setup
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+client.on('error', err => console.error(err));
+
+// API Routes
+
+
+// Make sure the server is listening for requests
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
